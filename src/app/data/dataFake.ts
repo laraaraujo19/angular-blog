d90462a158c45145c30f40d828b3536366a212d4
Lara <lara.araujo20@hotmail.com>
2024-02-25 19:47:33 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"NOVO HOMEM DE FERRO",
+        "description": "Marvel anuncia novo filme do homem de ferro, confira",
+        "photoCover":"https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg"
+    },
+    {
+        "id":"2",
+        "title":"Nova série anunciada no Disney+",
+        "description": "ola",
+        "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    }
+]
